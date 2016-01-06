3110a57e84031fcaeef2e0790401895d1498fd76
xqw1008 <taxueliu@qq.com>
2016-01-06 19:27:50 +0800

    1.4.4.2016010611

---
+++ application/controllers/register.php
@@ -0,0 +1,9 @@
+<?php
+class Register extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+   public function index(){
+       $this->load->view('register.html');
+   } 
+}
