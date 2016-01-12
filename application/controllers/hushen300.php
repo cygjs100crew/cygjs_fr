743ac375d2f517c00f468544c45f8c2662fb1e7b
huizheshijie <380003832@qq.com>
2016-01-12 18:40:03 +0800

    1.5.5.2016011201
    1、多联图表更换成echarts3.0折线图。

--- application/controllers/hushen300.php
+++ application/controllers/hushen300.php
@@ -11,29 +11,35 @@ class hushen300 extends CI_Controller{
 
     	//$list   = M('ip_liuliang');
        // $list =$list->select();
-        $list=$this->db->get('cy_ip_liuliang')->result_array();
-        foreach($list as $k=>$v){
-            // $timedata[$k] = date('Y-m-d',$v['time']);
-            $timedata[$k] = $v['timedata'];
-            $ipdata[$k] = $v['ipdata'];
-            $pudata[$k] = $v['pudata'];
-            $pvdata[$k] = $v['pvdata'];
-            $result['ipdata'] = implode(',', $ipdata);
-            $result['pudata'] = implode(',', $pudata);
-            $result['pvdata'] = implode(',', $pvdata);
-            $result['timedata'] = "'".implode("','", $timedata)."'";
+        $list=$this->db->get('data_source')->result_array();
+        // foreach($list as $k=>$v){
+        //     // $timedata[$k] = date('Y-m-d',$v['time']);
+        //     $timedata[$k] = $v['timedata'];
+        //     $ipdata[$k] = $v['ipdata'];
+        //     $pudata[$k] = $v['pudata'];
+        //     $pvdata[$k] = $v['pvdata'];
+        //     $result['ipdata'] = implode(',', $ipdata);
+        //     $result['pudata'] = implode(',', $pudata);
+        //     $result['pvdata'] = implode(',', $pvdata);
+        //     $result['timedata'] = "'".implode("','", $timedata)."'";
           
+        // }
+
+        foreach($list as $k=>$v){
+
+            // $Kdata[$k] ='['.$v['open'].','.$v['close'].','.$v['high'].','.$v['low'].']';
+            $Kdata[$k] =$v['current'];
+            $data_date[$k] ='"'.$v['data_date'].$v['data_time'].'"';
+
+            $result['data_date'] = implode(',', $data_date);
+            $result['ipdata'] = implode(',', $Kdata);
+
         }
+
 
        // $this->assign('conn', $result);
         $this->load->view('hushen300.html',$result);
-        var_dump($result);
-
-
-
-
-
-        $this->load->view('hushen300.html');
+        // var_dump($result['ipdata']);
     }
     public function data_add(){
     	$data = array(
@@ -53,8 +59,9 @@ class hushen300 extends CI_Controller{
 
         	// }
 
+            echo "成功";
 
 
-        	echo "今天是 " . date("Y-m-d h:i:s") .strtotime("2016-01-11 14:07:46").date("Y-m-d H:i:s",1452495932); "";
+        	// echo "今天是 " . date("Y-m-d h:i:s") .strtotime("2016-01-11 14:07:46").date("Y-m-d H:i:s",1452495932); "";
     }
 }
