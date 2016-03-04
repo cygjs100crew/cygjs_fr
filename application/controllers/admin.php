9447bd6632daf32d70d61b80f38966241e75d5b3
huizheshijie <380003832@qq.com>
2016-03-04 18:34:02 +0800

    1.17.26.2016030411
    新增：后台删除

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -46,6 +46,58 @@ class Admin extends MY_Controller{
     	$data['lishi'] = $this->db->limit(10)->order_by("id","desc")->get_where('investor_detail',array('symbol'=>"XAU"))->result_array(); // 查询历史交易
     	$this->load->view('admin/charts_index.html',$data);//前端在某个地方输出$username  
     }
+    /**
+     * 会员状态修改
+     */
+    public function changeStatus($method=null,$id=0){
+		$method=$_GET['method'];
+		$id=$_GET['id'];
+    	switch ( strtolower($method) ){
+            case 'forbiduser':
+                
+                break;
+            case 'resumeuser':
+                
+                break;
+            case 'deleteuser':
+                                $result=$this->db->delete('customer', array('id' => $id));
+                                if($result>0){
+                                    echo json_encode(array('status'=>1,'info'=>'删除成功'));
+                                } else {
+                                    echo json_encode(array('status'=>0,'info'=>'删除失败'));
+                                }
+                break;
+            default:
+                echo json_encode(array('status'=>0,'info'=>'参数非法'));
+        }
+    	
+        
+    	
+        // $id = array_unique((array)I('id',0));
+        // $id = is_array($id) ? implode(',',$id) : $id;
+        // if ( empty($id) ) {
+        //     $this->error('请选择要操作的数据!');
+        // }
+        // $map['id'] =   array('in',$id);
+        // switch ( strtolower($method) ){
+        //     case 'forbiduser':
+        //         $this->forbid('Huodong', $map );
+        //         break;
+        //     case 'resumeuser':
+        //         $this->resume('Huodong', $map );
+        //         break;
+        //     case 'deleteuser':
+        //                         $Model = M('Huodong');
+        //                         if($Model->where($map)->delete()){
+        //                             $this->success('删除成功');
+        //                         } else {
+        //                             $this->error('删除失败！');
+        //                         }
+        //         break;
+        //     default:
+        //         $this->error('参数非法');
+        // }
+    }
  
     
     //用户注册的地方，假设用户表中有这几个字段，用户名，密码，确认密码，手机号,验证码
