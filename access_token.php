4f1c24a0c1df14b126878a14b7d79795a0c59026
huizheshijie <380003832@qq.com>
2016-03-08 14:05:59 +0800

    1.17.28.2016030807
    同步

--- access_token.php
+++ access_token.php
@@ -1,1 +1,5 @@
-<?php exit();?>{"expire_time":1457410333,"access_token":"jAmnvR4u8ykM6IiX6EdlE6C1ugdemKjMHFt1vx-bRynqZmh_yWvZCDChnOCfxzMm2pdC3jgP0tjl8t-Be3634-XMe4zeH4QF2taHaqEc59U6htXVFogrZXr7zrKhy6eXJNAjAJADHU"}
+<<<<<<< HEAD
+<?php exit();?>{"expire_time":1457423073,"access_token":"O0-izhx0xH_8T1OUDp9o-0x5X_JHzu3rIC_H0ek2dKtHBlsQmL0dYcCV1rATrjrTA_D5citrISmaraReoQJj0Yy3GTkk1ijgR1JecgD4e3Gs_7XZCaDhDGJ3jPKbl2aGTBPaAAAXFT"}
+=======
+<?php exit();?>{"expire_time":1457423478,"access_token":"ACMHPEyDSBLLSl8VdP-K5NOGpRboK3hl8gqqk-PulgofwsUcZjFk5vVWBm8dx-1puWMMqy1fzZ5edIh8pBxBmmBO8HuE730z0KQC2QQC4Yo-QfDCNXxeBxyGkKyeO_TWQHNhAEAMDJ"}
+>>>>>>> origin/master
--- jsapi_ticket.php
+++ jsapi_ticket.php
@@ -1,1 +1,5 @@
-<?php exit();?>{"expire_time":1457410333,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VJHlzD0w5caAa3a37tv3ZPTWegzgEW9cZH7qVUqIuq3UDci_GND2Bt-l7te-9enUlw"}
+<<<<<<< HEAD
+<?php exit();?>{"expire_time":1457423073,"jsapi_ticket":"kgt8ON7yVITDhtdwci0qeYKqiCowM_vqoC7bgisRa_7ZjNNLW4lVga7nibGBKVF_TVcpwRhWTDv2G03CUEKK7A"}
+=======
+<?php exit();?>{"expire_time":1457423478,"jsapi_ticket":"kgt8ON7yVITDhtdwci0qeYKqiCowM_vqoC7bgisRa_7ZjNNLW4lVga7nibGBKVF_TVcpwRhWTDv2G03CUEKK7A"}
+>>>>>>> origin/master
