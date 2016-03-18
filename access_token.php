49a7f1ee0c5afd9ea6996895013a0b9ad1a680b8
huizheshijie <380003832@qq.com>
2016-03-18 13:23:01 +0800

    1.17.33.2016031807

--- access_token.php
+++ access_token.php
@@ -1,1 +1,9 @@
-<?php exit();?>{"expire_time":1458046784,"access_token":"gXlsSqjof6dZ1f1NF4GZw2DCvwQdEsFvAfmZH9s1C45Ne-IhDwgy0lcb-yagPeGshaSNZIrSP1thvCYa3QkVYRSsB1b6cbWnpvaFpY50HMnesV47nzkwhbQndxkuIEJGSIScAHAVLV"}
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php exit();?>{"expire_time":1458273581,"access_token":"VC82pQt8Aa84MqsMmEPT12uZ21jfU5J6N4SRCFsmm0KNWqqQDVT4ZP3PG0pjrsD4QCTVMfmzflQ3_QomMYsQkiE0DAgujCe3OgAzY5mC2K_E3UUdqfg10wecIDjzwNJ1KEEjAJAYSS"}
+=======
+<?php exit();?>{"expire_time":1458279233,"access_token":"dyHphVuKIk3JszkEfHGMNqOFUwaQvN9uTdxMxIbt8u4P6RIWnEwZWSS0wnXuegGyVxGNrljLm25JLgeRlDSoQegnRg-_C6tnxijo0XfB9svS6hsOQ9iRrtHaKXoDpI4lIICcAAAAAU"}
+>>>>>>> origin/master
+=======
+<?php exit();?>{"expire_time":1458279233,"access_token":"dyHphVuKIk3JszkEfHGMNqOFUwaQvN9uTdxMxIbt8u4P6RIWnEwZWSS0wnXuegGyVxGNrljLm25JLgeRlDSoQegnRg-_C6tnxijo0XfB9svS6hsOQ9iRrtHaKXoDpI4lIICcAAAAAU"}
+>>>>>>> origin/master
--- jsapi_ticket.php
+++ jsapi_ticket.php
@@ -1,1 +1,9 @@
-<?php exit();?>{"expire_time":1458046784,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUhUC6EP2jSSW4Z-KfJ__lIJrMNR6DWkls8Q9fmJRDW_dw"}
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php exit();?>{"expire_time":1458273581,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUgpZuWWB3odVV9r5l7lnH1OPcZJvol1-L9Ux0M_46Tkww"}
+=======
+<?php exit();?>{"expire_time":1458279234,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUgpZuWWB3odVV9r5l7lnH1OPcZJvol1-L9Ux0M_46Tkww"}
+>>>>>>> origin/master
+=======
+<?php exit();?>{"expire_time":1458279234,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUgpZuWWB3odVV9r5l7lnH1OPcZJvol1-L9Ux0M_46Tkww"}
+>>>>>>> origin/master
