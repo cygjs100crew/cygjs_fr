06b706f5cc6bfc0005811e11cd68bce4b2825da1
xqw1008 <taxueliu@qq.com>
2016-03-21 18:18:54 +0800

    1.17.34.2016032104

--- access_token.php
+++ access_token.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php exit();?>{"expire_time":1458530815,"access_token":"iGDy6kosBffRr91QGTcK87iMXFmTDCtn5Q7R-y-T3YW4gFl3PgZprQmG01s1XMGCw0QkTJNDAuDwB5ftB9SbHE-I_s0fv96tShjjmIxLn_iCJud_LxkQTzPP-g_7QkOXKORgABATXA"}
-=======
-<?php exit();?>{"expire_time":1458530301,"access_token":"NmZ8w9sw-cegedM02GPQyXLtEaYIEdOmI1lMhZC6nw_cBdLn7Dcpkqxc6Tp_wXNW9k8CexcVN9N8Lb7BH3qVZ2K9YbvOfyMifijkMox7L92NtTo5U_ASJvYpEELgYsU5DFBhAFAYAW"}
->>>>>>> origin/master
+<?php exit();?>{"expire_time":1458561632,"access_token":"5SpbJfWBXMC2R5_zkMZ8AF41fhZ3_uh_QfdtwIFlgDeeMI0AjSQrKmcVPFW8J6iQy0yHQ-vxbz6XYzNn9Ydt3NhbkpcIlpCoHtTi320iY4p4U5w_3r22lrCGWvHz45irBQRcADAODE"}
--- jsapi_ticket.php
+++ jsapi_ticket.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php exit();?>{"expire_time":1458530815,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUiOaXVUFnTiwvYiDuXH9bWOdIWhCFC3eyhcfTWLcQmL4A"}
-=======
-<?php exit();?>{"expire_time":1458530301,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUiOaXVUFnTiwvYiDuXH9bWOdIWhCFC3eyhcfTWLcQmL4A"}
->>>>>>> origin/master
+<?php exit();?>{"expire_time":1458561633,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUgLJqx8WhQU-1LNyxQWikNCNhAl6TfpwU1KYqKkoGbuZA"}
