54f223afc85326affd656208da0678756c65df46
tomsmart <zhili_show@163.com>
2016-03-22 14:16:37 +0800

    20160322
    改正弹出窗口显示

--- access_token.php
+++ access_token.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php exit();?>{"expire_time":1458616329,"access_token":"gvo61Qunyx9z2I6B6nOk3FnVcTbEsM9VlpqNBPEDKWYyeOC5iUyNYtHBnvshHflj7baohlO7GeLiviukoyArzlYJ7WKDJcxu2olW0WwqO3KIi6_1k_zUWUZcJFIlx5BlHOJhAEADKF"}
-=======
-<?php exit();?>{"expire_time":1458562664,"access_token":"_9CWijvOtcO9Rfed5HQBMM5S8HnjZAQxGUvj6w96_6ZhaPz7XCeMAW3it5q10sJkkiaDgJD25KaRjgQnSFxIipNCUu01vatYGQD1ne6jOgPi650o_u-j1FdhBpdYw7ojYZJiAHAGHR"}
->>>>>>> origin/master
+<?php exit();?>{"expire_time":1458632546,"access_token":"8lq_K1PP-0ztvFf5YXXCTQY95pU2FbPlEVdVv8-tAaX0OfeKi87Ezx37afJuFKUE7gIH_L_usZcmJFVefKwkQrVfORi3EmbZW3eBv4_6SjJ02HNhNP2TaYJWigvog5HZTVScABAPRO"}
--- jsapi_ticket.php
+++ jsapi_ticket.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php exit();?>{"expire_time":1458616329,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUg8Kb3n-BA3d1iaxBP9G-N6gvMDS1L22yCPNtiaDqioUQ"}
-=======
-<?php exit();?>{"expire_time":1458562664,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUgLJqx8WhQU-1LNyxQWikNCNhAl6TfpwU1KYqKkoGbuZA"}
->>>>>>> origin/master
+<?php exit();?>{"expire_time":1458632546,"jsapi_ticket":"sM4AOVdWfPE4DxkXGEs8VCLIdgjYiTD8bFeqKirDLUhPVoUpgKODwMsvto8QBZ_c-f9Nhc9qUEXBWqPnOrtytg"}
